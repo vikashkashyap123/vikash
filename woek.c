969c15c86e02c2ffbf064bfd307365d1d32c1edf
vikash kumar <vikashgupta14955@gmail.com>
2021-12-11 14:35:11 +0530

    Commit message

---
+++ woek.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello");
+    printf(" my name is vikash kumar ");
+    printf(" my branch is information technology ");
+    printf(" I belongs to samastipur,Bihar ");
+    printf("  my hobby is to listen music ,playing cricket ");
+    printf(" I want to be a software engineer ");
+
+    return 0;
+}
